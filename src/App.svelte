f56b074e5dd945e5089a2c2d83486b6ade67a434
Daniel Macák <daniel.macak@email.cz>
2019-10-12 19:37:07 +0200

    Migrated to svelte

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { slidePuzzle } from "./solution.mjs";
+	import { puzzleGenerator } from "./puzzleGenerator.mjs";
+
+	let name = 'world';
+
+	console.log(puzzleGenerator(3,3))
+</script>
+
+<h1>Hello {name}!</h1>
